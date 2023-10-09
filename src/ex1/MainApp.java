d26431250bce50cf36022955174dc7738d229749
Dona-A <u.dona@yahoo.com>
2023-10-09 17:43:14 +0300

    kj

--- src/ex1/MainApp.java
+++ src/ex1/MainApp.java
@@ -1,2 +1,38 @@
-package ex1;public class MainApp {
+package ex1;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainApp {
+    public static void parabola()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a1=");
+        int a=scanner.nextInt();
+        System.out.print("b1=");
+        int b=scanner.nextInt();
+        System.out.print("c1=");
+        int c=scanner.nextInt();
+        scanner.close();
+        int x2,y2;
+        x2=-b/(2*a);
+        y2=-(b+4*a*c)/4*a;
+        System.out.print(x2+ "," + y2);
+    }
+
+    public static void main(String[] args) {
+       int x, y,x1,y1;
+        List<Variabila> Variabile=new ArrayList<Variabila>();
+        Variabile.add(new Variabila(1,2,3));
+        Variabile.add(new Variabila(3,6,8));
+        Variabile.add(new Variabila(6,5,9));
+
+        System.out.println("Lista contine variabilele:");
+        for(Variabila p:Variabile) {
+            System.out.println(p);
+            p.Vf(p);
+        }
+    parabola();
+
+    }
 }
--- src/ex1/parabola.java
+++ src/ex1/parabola.java
@@ -1,2 +1,42 @@
-package ex1;public class parabola {
+package ex1;
+
+class Variabila {
+    private final int a;
+    private final int b;
+    private final int c;
+    public int x1,y1;
+    public Variabila(int a, int b, int c) {
+        super();
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+    }
+        public String toString() {
+            return "f(x)" + "=" + a + "x^2" + "+" + b + "x" + "+" + c;
+        }
+
+        public int get()
+        {
+            return a;
+        }
+        public int get1()
+         {
+          return b;
+         }
+        public int get2()
+        {
+         return a;
+        }
+
+        public  void Vf(Variabila p)
+        {
+            int x1,y1;
+            x1= -(p.get1()/2*p.get());
+            y1= (-p.get1()*p.get1()+4*p.get() *p.get2())/4*p.get();
+            System.out.println("Varful parabolei este:" + x1 + "," +y1);
+        }
+
+
+
 }
